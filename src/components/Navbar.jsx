e96bca66acd3ce0ca0fb0382acb3a4eaec65397a
fortune <omorodion.fortune@gmail.com>
2023-04-10 12:49:45 +0100

    navbar completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,18 @@
+import ImageLogo from "../assets/logo.svg"
+
 function Navbar() {
   return (
-    <nav className="">
-      <li>Home</li>
-      <li>News</li>
-      <li>About</li>
+    <nav className="py-3">
+      <div className="flex justify-between items-center">
+        <img className="px-5 " src={ImageLogo} alt="logo" />
+        <div className="flex space-x-5 gap-2 cursor-pointer px-40">
+          <p className=" hover:text-red-500 ">Home</p>
+          <p className=" hover:text-red-500">New</p>
+          <p className=" hover:text-red-500">Popular</p>
+          <p className=" hover:text-red-500">Trending</p>
+          <p className=" hover:text-red-500">Categories</p>
+        </div>
+      </div>
     </nav>
   );
 }
